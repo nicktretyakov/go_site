04f9d3bea7889dff719ad418ae7c6518c4faa744
Третьяков Николай <58301036+nicktretyakov@users.noreply.github.com>
2021-10-06 18:39:30 +0300

    Update server.go

--- pkg/website/server/server.go
+++ pkg/website/server/server.go
@@ -3,10 +3,7 @@ package server
 import (
 	"github.com/gofiber/fiber/v2"
 
-	"website.friendsonly.me/pkg/tools/httputils"
-	"website.friendsonly.me/pkg/website/config"
-	"website.friendsonly.me/pkg/website/handlers"
-)
+	)
 
 // BindHandlers provide
 func BindHandlers(app *fiber.App, publicHandler *handlers.PublicHandler, cfg *config.Config) {
